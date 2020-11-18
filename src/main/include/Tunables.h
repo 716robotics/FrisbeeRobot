01c1d2f17be84844237f68e4f2a9dc32b4101800
cannotilever <cannotilever@protonmail.com>
2020-11-18 14:52:42 -0500

    updated mappings

--- src/main/include/Tunables.h
+++ src/main/include/Tunables.h
@@ -1,5 +1,8 @@
 //716 Frisbee Robot - demonstration code tunables file
 
 //--Mappings--
-
+#define     PWMCHANNELDRIVEL1   0
+#define     PWMCHANNELDRIVEL2   1
+#define     PWMCHANNELDRIVER1   2
+#define     PWMCHANNELDRIVER2   3
 //--Multipliers--
